68ec7d95d044ba40993d03f6e6bac44632b82cd6
alkaaithal <52061895+alkaaithal@users.noreply.github.com>
2019-06-24 13:51:45 +0530

    Add files via upload
    added unique functionality

--- StoreCSV.py
+++ StoreCSV.py
@@ -18,11 +18,15 @@ for filename in all_files:
     frame = pd.concat(li, axis=0, ignore_index=True)
     records_ = df.to_dict(orient='records')
     for i in records_:
-        x = db.company_info.insert_one({"symbol": i['SYMBOL'], "series": i[' SERIES']})
-        db.stock_info.insert_one({"company_id": x.inserted_id, "date1": i[' DATE1'], "prev_close": i[' PREV_CLOSE'], "open_price": i[' OPEN_PRICE'], "high_price": i[' HIGH_PRICE'], "low_price": i[' LOW_PRICE'], "last_price": i[' LAST_PRICE'], "close_price": i[' CLOSE_PRICE'], "avg_price": i[' AVG_PRICE']})
+
+        x = db.company_info.insert_one({"symbol": i['SYMBOL'], "series": i[' SERIES'], "date1": i[' DATE1']})
+        db.company_info.create_index(
+            [("symbol", pymongo.DESCENDING), ("series", pymongo.ASCENDING), ("date1", pymongo.DESCENDING)],
+            unique=True
+        )
+        db.stock_info.insert_one({"company_id": x.inserted_id, "prev_close": i[' PREV_CLOSE'], "open_price": i[' OPEN_PRICE'], "high_price": i[' HIGH_PRICE'], "low_price": i[' LOW_PRICE'], "last_price": i[' LAST_PRICE'], "close_price": i[' CLOSE_PRICE'], "avg_price": i[' AVG_PRICE']})
         db.trade_info.insert_one({"company_id": x.inserted_id, "ttl_trd_qnty": i[' TTL_TRD_QNTY'], "turnover_lacs": i[' TURNOVER_LACS'], "no_of_trades": i[' NO_OF_TRADES'], "deliv_qty": i[' DELIV_QTY'], "deliv_per": i[' DELIV_PER']})
 
-    #lookup usage
 db.company_info.aggregate([
     {
         "$lookup":
